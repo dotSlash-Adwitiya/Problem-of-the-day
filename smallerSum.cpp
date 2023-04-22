7ec672acc295b48f17f3c9fd70a1f62f141ae386
Adwitiya Mourya <95630928+dotSlash-Adwitiya@users.noreply.github.com>
2023-04-22 18:51:00 +0530

    Optimal Solution added
    Optimal Solution can be achieved using O(n) Space complexity and O(nlogn) Time complexity

--- smallerSum.cpp
+++ smallerSum.cpp
@@ -16,6 +16,33 @@ vector<long long> smallerSum(int n,vector<int> &arr){
     return ans;
 }
 
+vector<long long> smallerSum(int n,vector<int> &arr){
+       
+    //* STEP I: Copy the original array and sort it 
+    vector<int> copy;
+    copy = arr;
+    vector<long long> ans;
+    unordered_map<long long, long long> hashMap;
+
+    sort(copy.begin(), copy.end());
+    long long sum = 0;
+
+    //* STEP - II: Look for each value in hashMap
+    for(int i = 0; i < n; i++) {
+        if(hashMap.find(copy[i]) == hashMap.end()){ //* If not found then assign sum till that value
+            hashMap[copy[i]] = sum;
+        }
+        sum += copy[i];     //* SUM will increment in every step
+    }
+
+
+    for(int &i : arr){
+        long long temp = hashMap.at(i);
+        ans.push_back(temp);
+    }
+    return ans;
+}
+
 int main ()
 {
     vector<int> v = {12 ,4, 6, 9, 4, 6};
