a0dd88c976da037b2dcc76c8ceaf756a1e339489
Adwitiya Mourya <95630928+dotSlash-Adwitiya@users.noreply.github.com>
2023-05-21 17:09:58 +0530

    Date : 21-May-2023
    Problem Link : https://practice.geeksforgeeks.org/problems/c6ced401352fd126b89129cd562a9247f057e40e/1

---
+++ busConductor.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findMoves(int n, vector<int> chairs, vector<int> passengers) {
+    
+    sort(chairs.begin(), chairs.end());
+    sort(passengers.begin(), passengers.end());
+    
+    int ans = 0;
+    
+    for(int i = 0; i < n; ++i)
+        ans += abs(chairs[i] - passengers[i]);
+    
+    return ans;
+}
+
+int main ()
+{
+
+  return 0;
+}
