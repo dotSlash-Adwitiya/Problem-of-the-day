43d3b69787efa5139d07502c809d1ac52ccb820e
Adwitiya Mourya <95630928+dotSlash-Adwitiya@users.noreply.github.com>
2023-06-07 22:59:17 +0530

    Brute Force approach added

--- leastPrimeNo.cpp
+++ leastPrimeNo.cpp
@@ -1,6 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//* BRUTE FORCE Approach - TC : O(n^2), SC O(1)
+vector<int> leastPrimeFactor(int n) {
+    vector<int> ans = {0,1};
+
+    if(n == 2){
+      ans.push_back(2);
+      return ans;
+    }
+
+    for(int i = 2; i <= n; i++) {
+      for(int j = 2; j <=n; j++)
+        if(i%j == 0) {
+          ans.push_back(j);
+          break;
+        }
+    }
+    return ans;
+}
+
 //* More Optimal Approach Using (Sieve Of Erastosthenes) TC : O(nlogn), SC : O(n)
 vector<int> leastPrimeFactor(int n) {
          //* By default values are marked as false
