3595b63960e73c41c859fb4162bc3166dc15dcb2
Adwitiya Mourya <95630928+dotSlash-Adwitiya@users.noreply.github.com>
2023-06-07 22:58:38 +0530

    PoTD - 07/06/2023
    Sieve of Erastosthene Application

---
+++ leastPrimeNo.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//* More Optimal Approach Using (Sieve Of Erastosthenes) TC : O(nlogn), SC : O(n)
+vector<int> leastPrimeFactor(int n) {
+         //* By default values are marked as false
+  vector<int> sieve(n+1);
+
+  //*Mark all of them as PRIME
+  for(int i = 1; i<=n; i++)
+    sieve[i] = i;
+
+  //* Now applying the sieve of erastosthenes :
+  for(int i = 2; i*i <=n; i++) 
+    if(sieve[i] == i)   
+      for(int j = i*i; j <= n; j+=i) 
+        if(sieve[j] == j)
+          sieve[j] = i;
+      
+    return sieve;
+}
+
+
+
+int main()
+{ 
+  cout << 4 % 2 << endl;
+  
+  return 0;
+}
