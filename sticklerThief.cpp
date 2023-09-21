b6524c2a5dd49f6c2472731b3ad8943174675770
Adwitiya Mourya <adwitiya.code@gmail.com>
2023-09-21 14:13:59 +0530

    PoTD - 21-Sept-2023 Memoization approach - DP

--- sticklerThief.cpp
+++ sticklerThief.cpp
@@ -1,6 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+//* THOUGHT Process to come up with recursive solution - 
+// * Ese toh kitne bi elements ho skte h jo bade ho, chote ho !
+//* Sabko kese include or exclude ek ek krke dekha ja skta h !! ??
+
+//* Jab sare elements ko include exclude krne ka case aae or bhot sare elements ban reh ho, tab 
+//* You can utilise recursion
 int recursion(int arr[], int n, int index)
 {
     // Base case
@@ -15,6 +22,31 @@ int recursion(int arr[], int n, int index)
     int notTaking = recursion(arr, n, index + 1);
 
     return max(taking, notTaking);
+}
+
+//* First ever DP Optimisation by MYSELF !!! (After seeing the recursive solution !)
+//* MEMOIZATION - 
+int recursion(int arr[], int n, int index, vector<int> &dp)
+{
+  // Base case
+  if (index >= n) {
+      return 0;
+  }
+
+  if(dp[index] != 0)
+      return dp[index];
+  // Taking the current house
+  int taking = arr[index] + recursion(arr, n, index + 2, dp);
+  // Not taking the current house
+  int notTaking = recursion(arr, n, index + 1, dp);
+  dp[index] = max(taking, notTaking);
+  return dp[index];
+}
+
+int FindMaxSum(int arr[], int n)
+{
+  vector<int> dp(n+1, 0);
+  return recursion(arr, n, 0, dp);
 }
 
 int FindMaxSum(int arr[], int n)
