158769d93090b6fabd4852caf4774459c9467ff0
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-05-09 13:28:45 +0530

    Leetcode-PoTD-09-May-2024, Added ordered Map approach

--- maxHappinessOfChildren.cpp
+++ maxHappinessOfChildren.cpp
@@ -20,3 +20,27 @@ long long maximumHappinessSum(vector<int>& happiness, int k) {
   }
   return maxHappiness;
 }
+
+// * Using Ordered Map - Same TC, but S.C : O(N)
+// * Revised it.rbegin() and it.rend() functions
+// * When using auto then access through '->' operator not the '.' dot operator.
+long long maximumHappinessSum(vector<int>& happiness, int k) {
+
+  map<int, int> mp;
+  
+  for(auto it : happiness)
+      mp[it]++;
+
+  int currHappiness = 0;
+  long long maxHappiness = 0;
+  int j = 0; // For keeping track of k
+  
+  for(auto it = mp.rbegin(); it != mp.rend() && j < k; it++, j++, currHappiness++ )
+      // * Checking for >= 0 because its mentioned, happiness won't decreased beyond 0.
+      // * CurrHappiness's value will inc in every element and will be subtracted from the actual value
+      maxHappiness += ((it->first - currHappiness) >= 0 ? (it->first - currHappiness) : 0 );
+  
+
+
+  return maxHappiness;
+}
