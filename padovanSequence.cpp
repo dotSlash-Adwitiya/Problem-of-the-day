f2d7fb8172e74f7585dfd0a8175b390d225dd09a
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-06-13 21:18:52 +0530

    Recursive Solution added

--- padovanSequence.cpp
+++ padovanSequence.cpp
@@ -2,10 +2,14 @@
 using namespace std;
 int mod = 1000000007;
 
-int padovanSequence(int n)
-{
+int padovanSequenceR(int n) {
   if(n == 0 || n == 1 || n == 2)
       return 1;
+  return padovanSequenceR(n-2) + padovanSequenceR(n-3);
+}
+
+int padovanSequence(int n)
+{
   
   vector<int> dp(n + 1,0);
   
