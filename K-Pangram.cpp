29cf2c1b2f460b808499118148131a6207f63dd4
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-07-26 23:48:31 +0530

    <PoTD/>:GfG - 26-July-2024

---
+++ K-Pangram.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool kPangram(string str, int k) {
+  int freq[26] = {0};
+  int cnt = 0, totalChar = 0;
+  
+  for(int i = 0; i<str.size(); i++){
+      // * Other way around, you can use another loop to cnt distinct characters.
+      if(freq[str[i] - 'a'] == 0)
+          cnt++;
+      if(str[i] - 'a' != ' '){ // * Size of arr 26, 32 - 26 = 6. so str[i] - 'a' = 32 will cause error 
+          totalChar++;   
+          freq[str[i] - 'a']++;
+      }
+  }
+  // * There has to be a total of 26 distinct chars
+  // * cnt is the no of distinct char already present in a string
+  // * 26 - cnt means the no of distinct char missing in the string.
+  // * if we can introduct missing characters in AT MOST K operations return true
+  if(totalChar == 26 && 26-cnt <= k)
+      return true;
+  return false;
+}
