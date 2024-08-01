af276d8251bdfc8425752b2abfa524ddd9a30120
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-08-01 23:59:55 +0530

    <PoTD/>:Leetcode - 01-Aug-2024

--- CountSeniorCitizens.cpp
+++ CountSeniorCitizens.cpp
@@ -10,3 +10,14 @@ int countSeniors(vector<string>& details) {
   }
   return seniorCount;
 }
+
+// * Shorter Code :
+int countSeniors(vector<string>& details) {
+  int seniorCnt = 0;
+  for(int i = 0; i < details.size(); i++) {
+      char age1 = details[i][11];
+      char age2 = details[i][12];
+      if(age1 > '6' or (age1 == '6' and age2 > '0')) seniorCnt++;
+  }
+  return seniorCnt;
+}
