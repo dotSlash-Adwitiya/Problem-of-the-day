20ca1f7ce336fe72e81d1a30f891ac94bff882d3
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-08-26 23:12:43 +0530

    <PoTD>:Leetcode - 26-Aug-2024

---
+++ n-ary-postorder.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+vector<int> ans;
+vector<int> postorder(Node* root) {
+    if(!root) return ans;
+    for(Node* &child  : root->children)
+        postorder(child);
+    ans.push_back(root->val);
+    return ans;
+}
