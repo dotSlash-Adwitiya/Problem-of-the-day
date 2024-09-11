b66d3b427986bd036dcc4b2e461270b44296b354
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-09-11 23:35:01 +0530

    <PoTD>:Leetcode-Bitmanipulation(EASY) - 11-Sept-2024

---
+++ MinBitFlips.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int minBitFlips(int start, int goal) {
+  int xorResult = start ^ goal;
+  int ans = 0;
+  
+  while (xorResult > 0) {
+      ans += xorResult & 1; 
+      xorResult >>= 1;
+  }
+  return ans;
+}
