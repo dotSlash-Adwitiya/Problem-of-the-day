8313e1c17a8261f8b71da4fc601f5f5601bed861
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-09-12 22:58:42 +0530

    <PoTD>:GfG(EASY) - 12-Sept-2024

--- middleOfLL.cpp
+++ middleOfLL.cpp
@@ -9,6 +9,14 @@ struct ListNode {
       ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+struct Node {
+      int data;
+      Node *next;
+      Node() : data(0), next(nullptr) {}
+      Node(int x) : data(x), next(nullptr) {}
+      Node(int x, Node *next) : data(x), next(next) {}
+};
+
 ListNode* middleNode(ListNode* head) {
   ListNode * slow = head, * fast = head;
 
@@ -23,4 +31,17 @@ ListNode* middleNode(ListNode* head) {
           fast = fast->next;
   }
   return slow;
+}
+
+// * PoTD Again: 12-Sept-2024
+int getMiddle(Node *head) {
+  if(!head) return 0;
+      
+  Node* slow = head, *fast = head;
+  
+  while(fast != NULL && fast->next != NULL){
+      fast = fast->next->next;
+      slow = slow->next;
+  }
+  return slow->data;
 }
