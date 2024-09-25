81a479029612f80efbe25feb0ace4acfc8da9f33
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-09-25 23:17:33 +0530

    <PoTD>:GfG(MEDIUM) - 25-Sept-2024

--- PalindromeLL.cpp
+++ PalindromeLL.cpp
@@ -31,3 +31,61 @@ bool compute(Node* head) {
   }
   return isPalindrome(pal);
 }
+
+// * PoTD: 25-Sept-2024
+int sizeOfLL(Node* head) {
+  int cnt = 0;
+  Node* ptr = head;
+  while(ptr != nullptr) {
+      cnt++;
+      ptr = ptr->next;
+  }
+  return cnt;
+}
+
+Node* reverseLinks(Node* &head) {
+  Node* prevPtr = nullptr, *currPtr = head, *fastPtr = head->next;
+  
+  while(currPtr != nullptr) {
+      currPtr->next = prevPtr;
+      prevPtr = currPtr;
+      currPtr = fastPtr;
+      if(fastPtr == nullptr)
+          break;
+      fastPtr = fastPtr->next;
+  }
+  return prevPtr;
+}
+
+bool isPalindrome(Node *head) {   
+  if(!head || !head->next)
+      return 1;
+  
+  //* Find size of LL
+  int n = sizeOfLL(head)/2, cnt = 0;
+  // cout << "size of LL : " << n << endl;
+  
+  
+  //* Traverse till size + 1
+  Node* firstPtr = head, *prevPtr = nullptr;
+  while(cnt != n) {
+      cnt++;
+      prevPtr = firstPtr;
+      firstPtr = firstPtr->next;
+  }
+  prevPtr->next = nullptr;
+
+  
+  //* Reverse all nodes after half of the LL is over
+  Node* lastPtr = reverseLinks(firstPtr);
+  
+  // cout << "Last Ptr data : " << lastPtr->data << endl;
+  firstPtr = head;
+  while(firstPtr != nullptr && lastPtr != nullptr) {
+      if(firstPtr->data != lastPtr->data)
+          return false;
+      firstPtr = firstPtr->next;
+      lastPtr = lastPtr->next;
+  }
+  return true;
+}
